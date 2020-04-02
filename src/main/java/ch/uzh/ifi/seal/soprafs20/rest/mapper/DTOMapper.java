ff17237e4a86773afabb7a21f3e4d285498dca86
Phi Kyuubi <david.steiger@uzh.ch>
2020-04-02 18:15:29 +0200

    Update DTOMapper.java

--- src/main/java/ch/uzh/ifi/seal/soprafs20/rest/mapper/DTOMapper.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/rest/mapper/DTOMapper.java
@@ -20,7 +20,7 @@ import org.mapstruct.factory.Mappers;
 public interface DTOMapper {
 
     DTOMapper INSTANCE = Mappers.getMapper(DTOMapper.class);
-    @Mapping(source = "avatar", target = "avatar")
+    @Mapping(source = "avatarId", target = "avatarId")
     @Mapping(source = "password", target = "password")
     @Mapping(source = "username", target = "username")
     User convertUserPostDTOtoEntity(UserPostDTO userPostDTO);
