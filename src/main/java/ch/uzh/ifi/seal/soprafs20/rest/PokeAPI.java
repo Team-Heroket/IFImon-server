ece0fe0566803945c8d898556442c159c7448937
TimDeanMoser <tim.moser@uzh.ch>
2020-04-24 12:19:57 +0200

    Catch PokeApi rate limit overflow by waiting with a thread
    Co-Authored-By: legudave <legudave@users.noreply.github.com>
    Co-Authored-By: nintend0d0 <nintend0d0@aholic.ch>

--- src/main/java/ch/uzh/ifi/seal/soprafs20/rest/PokeAPI.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/rest/PokeAPI.java
@@ -1,10 +1,12 @@
 package ch.uzh.ifi.seal.soprafs20.rest;
 
 import ch.uzh.ifi.seal.soprafs20.service.GameService;
+import ch.uzh.ifi.seal.soprafs20.objects.RequestTimer;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.lang.Thread;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -17,6 +19,10 @@ public class PokeAPI {
     // Not an object, just use static methods
 
     private final static Logger log = LoggerFactory.getLogger(PokeAPI.class);
+
+    private static RequestTimer requestTimer= new RequestTimer();
+    private static Thread requestThread = new Thread();
+
 
     // **** Settings
     private static String baseUrl = "https://pokeapi.co/api/v2/%s/%s/";
@@ -39,15 +45,35 @@ public class PokeAPI {
 
     public static JSONObject getFromURL(String strURL) {
 
-        
-
+        if(!requestThread.isAlive()){
+            requestThread=new Thread(requestTimer.resetTimer());
+            requestThread.start();
+        }
 
         log.debug(String.format("PokéAPI call %s.", strURL));
         JSONObject root = null;
+
+
+
+
         try {
+            if (!(requestThread.isAlive()&&requestTimer.getRequests()<97)){
+                try{
+                    int buffer=10;
+                    log.debug(String.format("PokéAPI limit reached, waiting %s", requestTimer.getTime()+buffer));
+                    Thread.sleep((requestTimer.getTime()+buffer)*1000);
+                    //requestThread=new Thread(requestTimer.resetTimer());
+                }catch(InterruptedException e){
+                    //stop
+                }
+            }
+
             URL url = new URL(strURL);
             JSONTokener tokener = new JSONTokener(url.openStream());
             root = new JSONObject(tokener);
+            requestTimer.addRequest();
+
+
         }
         // TODO: Handle this
         catch (MalformedURLException e) {
